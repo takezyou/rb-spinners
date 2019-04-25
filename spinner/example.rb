bfff758e8292b6c2d1df2b079cf73c4d7f3290c4
KaitoAkita <kaitokun07@icloud.com>
2019-04-26 03:50:54 +0900

    Change little code example.rb

--- spinner/example.rb
+++ spinner/example.rb
@@ -3,10 +3,15 @@ require 'tty-cursor'
 require './spinner'
 
 cursor = TTY::Cursor
-spinner_dots = Spinners::Dots
-# p spinner_dots.dig(:dots, :name)
-interval = spinner_dots.dig(:dots, :interval)
-frames = spinner_dots.dig(:dots, :frames)
+spinner = Spinners::SquareCorners
+
+puts spinner
+key = spinner.keys[0]
+
+# puts spinner.dig(key, :interval)
+# puts spinner.dig(key, :frames)
+interval = spinner.dig(key, :interval)
+frames = spinner.dig(key, :frames)
 
 STDOUT.write(cursor.hide)
 frames.each do |frame|
