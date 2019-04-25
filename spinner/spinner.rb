dd966e12aab55f97b9e44f8511221f7a40edb647
KaitoAkita <kaitokun07@icloud.com>
2019-04-26 03:49:53 +0900

    Add Spinners

--- spinner/spinner.rb
+++ spinner/spinner.rb
@@ -1,16 +1,283 @@
 module Spinners
   Dots = {
             dots: {
-              name: 'dots',
               interval: 80,
               frames: %w[⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏]
             }
           }
   Dots2 = {
             dots2: {
-              name: 'dots2',
               interval: 80,
               frames: %w[⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷]
             }
           }
+  Dots3 = {
+            dots3: {
+              interval: 80,
+              frames: %w[⠋ ⠙ ⠚ ⠞ ⠖ ⠦ ⠴ ⠲ ⠳ ⠓]
+            }
+          }
+  Dots4 = {
+            dots4: {
+              interval: 80,
+              frames: %w[⠄ ⠆ ⠇ ⠋ ⠙ ⠸ ⠰ ⠠ ⠰ ⠸ ⠙ ⠋ ⠇ ⠆]
+            }
+          }
+  Dots5 = {
+            dots5: {
+              interval: 80,
+              frames: %w[⠋ ⠙ ⠚ ⠒ ⠂ ⠂ ⠒ ⠲ ⠴ ⠦ ⠖ ⠒ ⠐ ⠐ ⠒ ⠓ ⠋]
+            }
+          }
+  Dots6 = {
+            dots6: {
+              interval: 80,
+              frames: %w[⠁ ⠉ ⠙ ⠚ ⠒ ⠂ ⠂ ⠒ ⠲ ⠴ ⠤ ⠄ ⠄ ⠤ ⠴ ⠲ ⠒ ⠂ ⠂ ⠒ ⠚ ⠙ ⠉ ⠁]
+            }
+          }
+  Dots7 = {
+            dots7: {
+              interval: 80,
+              frames: %w[⠈ ⠉ ⠋ ⠓ ⠒ ⠐ ⠐ ⠒ ⠖ ⠦ ⠤ ⠠ ⠠ ⠤ ⠦ ⠖ ⠒ ⠐ ⠐ ⠒ ⠓ ⠋ ⠉ ⠈]
+            }
+          }
+  Dots8 = {
+            dots8: {
+              interval: 80,
+              frames: %w[⠁ ⠁ ⠉ ⠙ ⠚ ⠒ ⠂ ⠂ ⠒ ⠲ ⠴ ⠤ ⠄ ⠄ ⠤ ⠠ ⠠ ⠤ ⠦ ⠖ ⠒ ⠐ ⠐ ⠒ ⠓ ⠋ ⠉ ⠈ ⠈]
+            }
+          }
+  Dots9 = {
+            dots9: {
+              interval: 80,
+              frames: %w[⢹ ⢺ ⢼ ⣸ ⣇ ⡧ ⡗ ⡏]
+            }
+          }
+  Dots10 = {
+              dots10: {
+                interval: 80,
+                frames: %w[⢄ ⢂ ⢁ ⡁ ⡈ ⡐ ⡠]
+              }
+            }
+  Dots11 = {
+              dots11: {
+                interval: 100,
+                frames: %w[⠁ ⠂ ⠄ ⡀ ⢀ ⠠ ⠐ ⠈]
+              }
+            }
+  Dots12 = {
+              dots12: {
+                interval: 80,
+                frames: [
+                   "⢀⠀",
+                   "⡀⠀",
+                   "⠄⠀",
+                   "⢂⠀",
+                   "⡂⠀",
+                   "⠅⠀",
+                   "⢃⠀",
+                   "⡃⠀",
+                   "⠍⠀",
+                   "⢋⠀",
+                   "⡋⠀",
+                   "⠍⠁",
+                   "⢋⠁",
+                   "⡋⠁",
+                   "⠍⠉",
+                   "⠋⠉",
+                   "⠋⠉",
+                   "⠉⠙",
+                   "⠉⠙",
+                   "⠉⠩",
+                   "⠈⢙",
+                   "⠈⡙",
+                   "⢈⠩",
+                   "⡀⢙",
+                   "⠄⡙",
+                   "⢂⠩",
+                   "⡂⢘",
+                   "⠅⡘",
+                   "⢃⠨",
+                   "⡃⢐",
+                   "⠍⡐",
+                   "⢋⠠",
+                   "⡋⢀",
+                   "⠍⡁",
+                   "⢋⠁",
+                   "⡋⠁",
+                   "⠍⠉",
+                   "⠋⠉",
+                   "⠋⠉",
+                   "⠉⠙",
+                   "⠉⠙",
+                   "⠉⠩",
+                   "⠈⢙",
+                   "⠈⡙",
+                   "⠈⠩",
+                   "⠀⢙",
+                   "⠀⡙",
+                   "⠀⠩",
+                   "⠀⢘",
+                   "⠀⡘",
+                   "⠀⠨",
+                   "⠀⢐",
+                   "⠀⡐",
+                   "⠀⠠",
+                   "⠀⢀",
+                   "⠀⡀"
+                 ]
+              }
+            }
+  Line = {
+            line: {
+              interval: 130,
+              frames: %w[- \\ | /]
+            }
+          }
+  Line2 = {
+            line2: {
+              interval: 100,
+              frames: %w[⠂ - – — – -]
+            }
+          }
+  Pipe = {
+            pipe: {
+              interval: 100,
+              frames: %w[┤ ┘ ┴ └ ├ ┌ ┬ ┐]
+            }
+          }
+  SimpleDots = {
+                  simpleDots: {
+                    interval: 400,
+                    frames: [
+                      ".  ",
+                      ".. ",
+                      "...",
+                      "   "
+                    ]
+                  }
+                }
+  SimpleDotsScrolling = {
+                          simpleDotsScrolling: {
+                            interval: 200,
+                            frames: [
+                              ".  ",
+                              ".. ",
+                              "...",
+                              " ..",
+                              "  .",
+                              "   "
+                            ]
+                          }
+                        }
+  Star = {
+            star: {
+              interval: 70,
+              frames: %w[✶ ✸ ✹ ✺ ✹ ✷]
+            }
+          }
+  Star2 = {
+            star2: {
+              interval: 80,
+              frames: %w[+ x *]
+            }
+          }
+  Flip = {
+            flip: {
+              interval: 70,
+              frames: %w[_ _ _ - ` ` ' ´ - _ _ _]
+            }
+          }
+  Hamburger = {
+                hamburger: {
+                  interval: 100,
+                  frames: %w[☱ ☲ ☴]
+                }
+              }
+  GrowVertical = {
+                    growVertical: {
+                      interval: 120,
+                      frames: %w[▁ ▃ ▄ ▅ ▆ ▇ ▆ ▅ ▄ ▃]
+                    }
+                  }
+  GrowHorizontal = {
+                      growHorizontal: {
+                        interval: 120,
+                        frames: %w[▏ ▎ ▍ ▌ ▋ ▊ ▉ ▊ ▋ ▌ ▍ ▎]
+                      }
+                    }
+  Balloon = {
+              balloon: {
+                interval: 140,
+                frames: [
+                  " ",
+                  ".",
+                  "o",
+                  "O",
+                  "@",
+                  "*",
+                  " "
+                ]
+              }
+            }
+  Ballon2 = {
+              balloon2: {
+                interval: 120,
+                frames: %w[. o O ° O o .]
+              }
+            }
+  Noise = {
+            noise: {
+              interval: 100,
+              frames: %w[▓ ▒ ░]
+            }
+          }
+  Bounce = {
+              bounce: {
+                interval: 120,
+                frames: %w[⠁ ⠂ ⠄ ⠂]
+              }
+            }
+  BoxBounce = {
+                boxBounce: {
+                  interval: 120,
+                  frames: %w[▖ ▘ ▝ ▗]
+                }
+              }
+  BoxBounce2 = {
+                  boxBounce2: {
+                    interval: 100,
+                    frames: %w[▌ ▀ ▐ ▄]
+                  }
+                }
+  Triangle = {
+                triangle: {
+                  interval: 50,
+                  frames: %w[◢ ◣ ◤ ◥]
+                }
+              }
+  Arc = {
+          arc: {
+            interval: 100,
+            frames: %w[◜ ◠ ◝ ◞ ◡ ◟]
+          }
+        }
+  Circle = {
+              circle: {
+                interval: 120,
+                frames: %w[◡ ⊙ ◠]
+              }
+            }
+  SquareCorners = {
+                    squareCorners: {
+                      interval: 180,
+                      frames: %w[◰ ◳ ◲ ◱]
+                    }
+                  }
+  CircleQuarters = {
+                      circleQuarters: {
+                        interval: 120,
+                        frames: %w[◴ ◷ ◶ ◵]
+                      }
+                    }
 end
